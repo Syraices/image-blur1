439d67f33080123c764edec800c334c450a98c4f
Shmulik <syraices@gmail.com>
2019-08-26 03:02:14 +0000

    floyd cycle detection

--- cycle.rb
+++ cycle.rb
@@ -9,29 +9,40 @@ end
 
 def double(list)
    list_b = list.next_node
-   while list
-    list = list.next_node
-    list_b = list_b.next_node.next_node
+
+  while list
+   
     puts list_b.value
     
     puts list.value
     puts '---'
-    if list != list_b
-      puts false
-    else 
-      puts true
+    # if list != list_b
+    #   puts 'no loop found'
+    # else 
+    if list == list_b
+      puts 'loop found'
       break
     end
-    
+    list = list.next_node
+    if list_b.next_node
+      list_b = list_b.next_node.next_node
+    else
+      puts 'no loop found'
+      break 
+    end
   end
 end
 
 
 
-node1 = LinkedListNode.new(37)
-node2 = LinkedListNode.new(99, node1)
-node3 = LinkedListNode.new(12, node2)
-node4 = LinkedListNode.new(68, node3)
-node1.next_node = node4
+node1 = LinkedListNode.new(8)
+node2 = LinkedListNode.new(7, node1)
+node3 = LinkedListNode.new(6, node2)
+node4 = LinkedListNode.new(5, node3)
+node5 = LinkedListNode.new(4, node4)
+node6 = LinkedListNode.new(3, node5)
+node7 = LinkedListNode.new(2, node6)
+node8 = LinkedListNode.new(1, node7)
+node1.next_node = node6
 
-puts double(node4)
+puts double(node8)
